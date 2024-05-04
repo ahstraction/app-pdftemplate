e051374f0d0845db4086db825930786d0cd424dd
ahstraction <aa66mona77@gmail.com>
2024-05-04 16:11:10 +0500

    "Added header and footer to each page, added extra page for each topic and set topic as footer for each page in the PDF"
    The changes include:
    
    * Adding a header to each page with the topic name

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fpdf import FPDF
 import pandas as pd
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 df= pd.read_csv("C:/Users/Dell/OneDrive/Documents/Python/app-pdftemplate/topics.csv")
+# Set the header 
 for index, row in df.iterrows():
     pdf.add_page()
 
@@ -9,6 +11,16 @@ for index, row in df.iterrows():
     pdf.set_text_color(0, 0, 0)
     pdf.cell(w=0,  h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10,21,200,21 )
+#Set the Footer
+    pdf.ln(265)
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180,180,180)
+    pdf.cell(w=0,h=10,txt=row["Topic"],align="R")
+# Added extra page    
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+        pdf.ln(265)
+        pdf.set_font(family="Times", style="I", size=8)
+        pdf.set_text_color(180,180,180)
+        pdf.cell(w=0,h=10,txt=row["Topic"],align="R")
 pdf.output("output.pdf")
