9cd581ef0d0e977dd426ce3bd249a5d6c30d5f13
ahstraction <aa66mona77@gmail.com>
2024-05-04 16:28:52 +0500

    Add horizontal lines every 20mm on each page; add horizontal lines every 15mm on the footer
    * Added a for loop to create horizontal lines every 20mm on each page
    * Added a for

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False, margin=0)
 df= pd.read_csv("C:/Users/Dell/OneDrive/Documents/Python/app-pdftemplate/topics.csv")
+
 # Set the header 
 for index, row in df.iterrows():
     pdf.add_page()
@@ -10,7 +11,8 @@ for index, row in df.iterrows():
     pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(0, 0, 0)
     pdf.cell(w=0,  h=12, txt=row["Topic"], align="L", ln=1)
-    pdf.line(10,21,200,21 )
+    for y in range(20,280,20):
+        pdf.line(10,y,200,y )
 #Set the Footer
     pdf.ln(265)
     pdf.set_font(family="Times", style="I", size=8)
@@ -23,4 +25,6 @@ for index, row in df.iterrows():
         pdf.set_font(family="Times", style="I", size=8)
         pdf.set_text_color(180,180,180)
         pdf.cell(w=0,h=10,txt=row["Topic"],align="R")
+        for y in range(20,280,15):
+            pdf.line(10,y,200,y )
 pdf.output("output.pdf")
